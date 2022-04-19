34cf7cfb8293d786acb54dbfffae419e3d60febd
Drew <DGriffin@appacademy.io>
2022-04-19 16:21:37 -0500

    App init

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(morgan('dev'));
+app.use(routes);
+
+app.use((req, res, next) => {
+    const err = new Error('The requested page couldn\'t be found.');
+    err.status = 404;
+    next(err);
+});
+
+// Custom error handlers.
+
+// Error handler to log errors.
+app.use((err, req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+        // TODO Log the error to the database.
+    } else {
+        console.error(err);
+    }
+    next(err);
+});
+
+// Error handler for 404 errors.
+app.use((err, req, res, next) => {
+    if (err.status === 404) {
+        res.status(404);
+        res.render('page-not-found', {
+            title: 'Page Not Found',
+        });
+    } else {
+        next(err);
+    }
+});
+
+// Generic error handler.
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    const isProduction = process.env.NODE_ENV === 'production';
+    res.render('error', {
+        title: 'Server Error',
+        message: isProduction ? null : err.message,
+        stack: isProduction ? null : err.stack,
+    });
+});
+
+module.exports = app;
