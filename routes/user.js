3883ead6fc3b7dbe1f53041778ebe81505ec6ea1
Drew <DGriffin@appacademy.io>
2022-04-19 20:50:22 -0500

    Persist user

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 
 const db = require('../db/models');
 const { csrfProtection, asyncHandler } = require('./utils');
+const { loginUser } = require('../auth');
 
 const router = express.Router();
 
@@ -83,6 +84,7 @@ router.post('/register', csrfProtection, userValidators,
             const hashedPassword = await bcrypt.hash(password, 10);
             user.hashedPassword = hashedPassword;
             await user.save();
+            loginUser(req, res, user);
             res.redirect('/');
         } else {
             const errors = validatorErrors.array().map((error) => error.msg);
@@ -134,6 +136,7 @@ router.post('/user/login', csrfProtection, loginValidators,
                     // If the password hashes match, then login the user
                     // and redirect them to the default route.
                     // TODO Login the user.
+                    loginUser(req, res, user);
                     return res.redirect('/');
                 }
             }
